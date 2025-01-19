e149e25df2aefdffeb70135fa2a4f00d1aecfa4a
Kelsey379 <kelseyroughpng@gmail.com>
2025-01-19 13:32:07 -0500

    Step 1: * added macID to pom.xml * parse for -i flag * logger calls

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -6,16 +6,33 @@ import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//import apache cli for part #1
+import org.apache.commons.cli.*;
+
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine commandLine = null;
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            // Try to get the maze file, otherwise catch exception
+            commandLine = parser.parse(parseInput(), args);
+            String mazeFile = null;
+            if (commandLine.hasOption('i')) {
+                mazeFile = commandLine.getOptionValue('i');
+            }
+
+            logger.info("**** Reading the maze from file " + mazeFile);
+            BufferedReader reader = new BufferedReader(new FileReader(mazeFile));
             String line;
+
+            //Note: I have decided to leave these System.out.print statements as they are not irrelevant at this point
+            // These are in use for indicating which file is being read, but this will be replaced as features are implemented
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
                     if (line.charAt(idx) == '#') {
@@ -26,11 +43,27 @@ public class Main {
                 }
                 System.out.print(System.lineSeparator());
             }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+        } catch (Exception e) {
+            logger.error("/!\\ An error has occured /!\\");
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+
+        logger.info("**** Computing path");
+        logger.error("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
     }
+
+    /**
+     * Parse the user input to find the desired maze file path
+     * @return the parsed options that users have entered
+     */
+    public static Options parseInput(){
+        Options options = new Options();
+
+        Option mazeFilePath = new Option("i", true, "Path to the file containing the maze");
+        mazeFilePath.setRequired(true);
+        options.addOption(mazeFilePath);
+
+        return options;
+    }
+
 }
