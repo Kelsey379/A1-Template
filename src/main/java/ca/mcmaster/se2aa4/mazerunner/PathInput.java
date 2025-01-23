d6d4d22ab90152e751f53095f87493a16778c523
Kelsey379 <kelseyroughpng@gmail.com>
2025-01-23 02:45:41 -0500

    *Reads user input and converts it to canonical form *To be refactored to account for spaces and errors *Closes #4

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathInput.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathInput.java
@@ -2,8 +2,8 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PathInput {
 
@@ -13,7 +13,7 @@ public class PathInput {
     }
 
 
-    // TODO: make sure this can account for spaces in the input, it currently does not
+    //TODO: make sure this can account for spaces in the input, it currently does not
 
     public static String canonizedPath(String pathInput){
 
@@ -21,20 +21,20 @@ public class PathInput {
 
         StringBuilder pathForAnalysis = new StringBuilder();
 
-//        Pattern pattern = Pattern.compile("(\\d+)([RLF])");
-//        Matcher matcher = pattern.matcher(pathInput);
-//
-//        while (matcher.find()){
-//            String coefficient = matcher.group(1);
-//            String pointedDirection = matcher.group(2);
-//            int repeatStep = Integer.parseInt(coefficient);
-//
-//            for (int i = 0; i < repeatStep; i++){
-//
-//                pathForAnalysis.append(pointedDirection);
-//
-//            }
-//        }
+        Pattern pattern = Pattern.compile("(\\d+)([RLF])");
+        Matcher matcher = pattern.matcher(pathInput);
+
+        while (matcher.find()){
+            String coefficient = matcher.group(1);
+            String pointedDirection = matcher.group(2);
+            int repeatStep = Integer.parseInt(coefficient);
+
+            for (int i = 0; i < repeatStep; i++){
+
+                pathForAnalysis.append(pointedDirection);
+
+            }
+        }
         return pathForAnalysis.toString();
     }
 
