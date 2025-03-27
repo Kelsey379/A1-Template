81861b971a2bb803b3e5e9aad3f2a16b49fc1086
Kelsey379 <kelseyroughpng@gmail.com>
2025-03-27 01:24:34 -0400

    Updated Tests (before full refactor/addition of design patterns)

--- src/test/java/MazeTest.java
+++ src/test/java/MazeTest.java
@@ -15,31 +15,39 @@ public class MazeTest {
         maze = new Maze("src/test/mazeSamples/testMaze.txt");
     }
 
-//    @Test
-//    public void testIsWall() {
-//
-//        //assertTrue(maze.isWall(0,0));
-//        assertFalse(maze.isWall(1,1));
-//    }
+    @Test
+    public void testCheckEmptyRow() {
+        //check for no empty rows
+        String path = maze.checkEmptyRow();
+        assertNotNull(path);
+        assertTrue(path.isEmpty());
+    }
+
+    @Test
+    public void testIsWall() {
+
+        assertTrue(maze.isWall(0,0));
+        assertFalse(maze.isWall(1,1));
+    }
 
     @Test
     public void testIsInBounds() {
-        assertTrue(maze.isInBounds(1,1));
+        assertTrue(maze.isInBounds(0,0));
         assertFalse(maze.isInBounds(100,100));
     }
 
     @Test
     public void testStartPosition() throws Exception {
         MapPosition start = Maze.getStartPosition();
-        assertEquals(new MapPosition(0,5, DirectionOrientation.Direction.EAST), start);
+        assertEquals(new MapPosition(1,6, DirectionOrientation.Direction.WEST), start);
     }
 
-//    @Test
-//    public void testEndPosition() throws Exception {
-//        MapPosition start = Maze.getStartPosition();
-//        MapPosition end = Maze.getEndPosition();
-//        assertEquals(new MapPosition(6,1, DirectionOrientation.Direction.EAST), end);
-//    }
+    @Test
+    public void testEndPosition() throws Exception {
+        MapPosition start = Maze.getStartPosition();
+        MapPosition end = Maze.getEndPosition();
+        assertEquals(new MapPosition(5,0, DirectionOrientation.Direction.WEST), end);
+    }
 
 
 }
